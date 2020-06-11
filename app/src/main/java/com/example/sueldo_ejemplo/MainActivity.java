9202be98728ff2eaf45028d164bd119cc6954203
unknown <daniel.munoz2@mail.udp.cl>
2020-06-10 23:58:15 -0400

    Update v1

--- app/src/main/java/com/example/sueldo_ejemplo/MainActivity.java
+++ app/src/main/java/com/example/sueldo_ejemplo/MainActivity.java
@@ -19,54 +19,64 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLimpiar, btnCalcular;
     private TextView lbl_pago, lbl_dcto;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtHoras = (EditText)findViewById(R.id.txtHoras);
-        txtDias = (EditText)findViewById(R.id.txtDias);
-        chbxPago = (CheckBox)findViewById(R.id.chbxPago);
-        chbxDcto = (CheckBox)findViewById(R.id.chbxDcto);
-        rgRedondeo = (RadioGroup)findViewById(R.id.rgRedondeo);
-        rbNoRedondeo = (RadioButton)findViewById(R.id.rbNoRedondeo);
-        btnLimpiar = (Button)findViewById(R.id.btnLimpiar);
-        btnCalcular = (Button)findViewById(R.id.btnCalcular);
-        lbl_dcto = (TextView)findViewById(R.id.lbl_dcto);
-        lbl_pago = (TextView)findViewById(R.id.lbl_pago);
-
+        txtHoras = (EditText) findViewById(R.id.txtHoras);
+        txtDias = (EditText) findViewById(R.id.txtDias);
+        chbxPago = (CheckBox) findViewById(R.id.chbxPago);
+        chbxDcto = (CheckBox) findViewById(R.id.chbxDcto);
+        rgRedondeo = (RadioGroup) findViewById(R.id.rgRedondeo);
+        rbNoRedondeo = (RadioButton) findViewById(R.id.rbNoRedondeo);
+        btnLimpiar = (Button) findViewById(R.id.btnLimpiar);
+        btnCalcular = (Button) findViewById(R.id.btnCalcular);
+        lbl_dcto = (TextView) findViewById(R.id.lbl_dcto);
+        lbl_pago = (TextView) findViewById(R.id.lbl_pago);
     }
 
-    public void calcular (View view){
+    public void limpiar(View view) {
+        txtDias.setText(String.valueOf(""));
+        txtHoras.setText(String.valueOf(""));
+        lbl_pago.setText(String.valueOf(""));
+        lbl_dcto.setText(String.valueOf(""));
 
+        if (rgRedondeo.getCheckedRadioButtonId() == R.id.rbRedondeo){
+            rbRedondeo.setChecked(false);
+        }
+        if (rgRedondeo.getCheckedRadioButtonId() == R.id.rbNoRedondeo){
+            rbNoRedondeo.setChecked(false);
+        }
+        if (chbxDcto.isChecked() == true){
+            chbxDcto.setChecked(false);
+        }
+        if (chbxPago.isChecked() == true){
+            chbxPago.setChecked(false);
+        }
+    }
+
+    public void calcular(View view) {
         int horas = Integer.parseInt(txtHoras.getText().toString());
         int dias = Integer.parseInt(txtDias.getText().toString());
-        int horas_mensuales = horas*dias;
-        double pago = horas_mensuales*10;
+        int horas_mensuales = horas * dias;
+        double pago = horas_mensuales * 10;
         double descuento = 0.00;
         double sueldo_base = 1000.00;
 
-        if(chbxPago.isChecked() == true){
+        if (chbxPago.isChecked() == true) {
             lbl_pago.setText(String.valueOf(pago));
         }
-        if(chbxDcto.isChecked() == true && pago >1000){
+        if (chbxDcto.isChecked() == true && pago > 1000) {
 
-            descuento = pago - (pago*0.1);
+            descuento = pago - (pago * 0.1);
             lbl_dcto.setText(String.valueOf(descuento));
         }
         if (rgRedondeo.getCheckedRadioButtonId() == R.id.rbRedondeo) {
-            int pago_redondeo = (int)Math.round(pago);
+            int pago_redondeo = (int) Math.round(pago);
             lbl_pago.setText(String.valueOf(pago_redondeo));
-            int dcto_redondeo = (int)Math.round((descuento));
+            int dcto_redondeo = (int) Math.round((descuento));
             lbl_dcto.setText(String.valueOf(dcto_redondeo));
         }
-
-
-    }
-
-    public void limpiar(View view){
-
     }
 }
