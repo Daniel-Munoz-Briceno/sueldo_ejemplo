647bbf7f858a8c34f741a1d6bb41f1a033c4a37c
unknown <daniel.munoz2@mail.udp.cl>
2020-06-12 23:42:39 -0400

    Refacto

--- app/src/main/java/com/example/sueldo_ejemplo/MainActivity.java
+++ app/src/main/java/com/example/sueldo_ejemplo/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.sueldo_ejemplo;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -11,16 +11,14 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.RoundingMode;
 
 public class MainActivity extends AppCompatActivity {
-    private RadioGroup RadioGroup;
     private EditText txtHoras, txtDescuento, txtSueldo;
     private CheckBox chbxDcto;
     private RadioGroup rgRedondeo;
     private RadioButton rbRedondeo, rbNoRedondeo;
-    private Button btnLimpiar, btnCalcular;
     private TextView lbl_pago, lbl_dcto;
-    private double D1, D2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,33 +29,28 @@ public class MainActivity extends AppCompatActivity {
         txtHoras = (EditText) findViewById(R.id.txtHoras);
         txtDescuento = (EditText) findViewById(R.id.txtDescuento);
         chbxDcto = (CheckBox) findViewById(R.id.chbxDcto);
-        btnLimpiar = (Button) findViewById(R.id.btnLimpiar);
-        btnCalcular = (Button) findViewById(R.id.btnCalcular);
         lbl_dcto = (TextView) findViewById(R.id.lbl_dcto);
         lbl_pago = (TextView) findViewById(R.id.lbl_pago);
-
-        //Limpiar y formatear variables
-        txtDescuento.setText(String.valueOf("0"));
-        txtSueldo.setText(String.valueOf(""));
-        txtHoras.setText(String.valueOf("160"));
-        lbl_pago.setText(String.valueOf("0"));
-        lbl_dcto.setText(String.valueOf("0"));
+        rgRedondeo = (RadioGroup) findViewById(R.id.rgRedondeo);
 
         //Funcion escucha RadioGroup
-        RadioGroup = (RadioGroup) findViewById(R.id.rgRedondeo);
-        RadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        rgRedondeo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup Group, int checkedID) {
+                double tD=0, tP=0;                    //Variables auxiliar contadores y valores totales acumulados
+
                 if (checkedID == R.id.rbRedondeo) {
+                    tD = Double.parseDouble(txtDescuento.getText().toString());
+                    tP = Double.parseDouble(txtSueldo.getText().toString());
+                    lbl_pago.setText(String.valueOf(Math.round(tP)));
+                    lbl_dcto.setText(String.valueOf(Math.round(tD)));
                     Toast.makeText(getApplication(), "Redondeo", Toast.LENGTH_SHORT).show();
                 } else if (checkedID == R.id.rbNoRedondeo) {
-                    Toast.makeText(getApplication(), "Sin Redondeo", Toast.LENGTH_SHORT).show();
-                    //EditText TD=(EditText) findViewById(R.id.lbl_dcto);
-                    //EditText TP=(EditText) findViewById(R.id.lbl_pago);
-                    double tD=Double.parseDouble(lbl_dcto.getText().toString());
-                    double tP=Double.parseDouble(lbl_pago.getText().toString());
-                    lbl_dcto.setText(String.valueOf(tD));
-                    lbl_pago.setText(String.valueOf(tP));
+                    tD = Double.parseDouble(txtDescuento.getText().toString());
+                    tP = Double.parseDouble(txtSueldo.getText().toString());
+                    lbl_pago.setText(String.valueOf(Math.ceil(tP)));
+                    lbl_dcto.setText(String.valueOf(Math.ceil(tD)));
+                    Toast.makeText(getApplication(), "Sin Redondeo" , Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -67,68 +60,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Limpiar pantalla
+    @SuppressLint("SetTextI18n")
     public void limpiar(View view) {
-        txtDescuento.setText(String.valueOf("0"));
-        txtSueldo.setText(String.valueOf(""));
-        txtHoras.setText(String.valueOf("160"));
-        lbl_pago.setText(String.valueOf("0"));
-        lbl_dcto.setText(String.valueOf("0"));
+        txtDescuento.setText("0");
+        txtSueldo.setText("");
+        txtHoras.setText("160");
+        lbl_pago.setText("0");
+        lbl_dcto.setText("0");
 
-
-        if (chbxDcto.isChecked() == false) {
+        if (!chbxDcto.isChecked()) {
             chbxDcto.setChecked(true);
         }
-/*
-        if (rgRedondeo.getCheckedRadioButtonId() == R.id.rbRedondeo){
-            rbRedondeo.setChecked(false);
-        }
-
-        if (rgRedondeo.getCheckedRadioButtonId() == R.id.rbNoRedondeo){
-            rbNoRedondeo.setChecked(false);
-        }
-
- */
-    }
-
-    public class GlobalInfo {
-
     }
 
     public void calcular(View view) {
         try {
             // stuff that could cause error
-            double sueldo = Double.valueOf(txtSueldo.getText().toString());
-            double descuento = Double.valueOf(txtDescuento.getText().toString());
-            double horas = Double.valueOf(txtHoras.getText().toString());
-            double afp_salud = (sueldo * 20) / 100;  //20% de impocisiones sobre el sueldo
-            double vH = (sueldo / 160);              //Valor por hora
-            double vD = (vH * 8);                    //Valor por dia
-            double vA1 = 0, vA2 = 0, tD = 0, tP = 0;          //Variables auxiliar contadores y valores totales acumulados
+            double S = Double.parseDouble(txtSueldo.getText().toString());
+            double D = Double.parseDouble(txtDescuento.getText().toString());
+            double horas = Double.parseDouble(txtHoras.getText().toString());
+            double iM = (S * 20) / 100;                     //20% de impocisiones sobre el sueldo
+            double vH = (S / 160);                          //Valor por hora
+            double vA1, vA2, tD=0, tP=0;                    //Variables auxiliar contadores y valores totales acumulados
 
-            if (chbxDcto.isChecked() == true) {             //Con descuento
+            if (chbxDcto.isChecked()) {                     //Con DESCUENTO
                 //horas
                 if (horas < 160) {
                     vA1 = (vH * horas);
-                    vA2 = (sueldo - vA1);
-                    tD = (tD + vA2 - descuento) * -1;           // Para que muestre valors con signo negativo
-                    tP = tP + vA1 - descuento;
+                    vA2 = (S - vA1);
+                    tD = (tD + vA2 + iM - D) * -1;           // Para que muestre valors con signo negativo
+                    tP = tP + vA1 - D;
                     lbl_dcto.setText(String.valueOf(tD));
                     lbl_pago.setText(String.valueOf(tP));
                 } else if (horas > 160) {
                     vA1 = (vH * horas);
-                    tD = (tD + vA1 - descuento - sueldo);       // signo negativo
-                    tP = tP + vA1 - descuento;
+                    tD = (tD + vA1 + iM - D - S);
+                    tP = tP + vA1 - D;
                     lbl_dcto.setText(String.valueOf(tD));
                     lbl_pago.setText(String.valueOf(tP));
                 } else {
-                    tD = (tD - descuento);
-                    tP = (tP + sueldo - descuento);
+                    tD = (tD + iM - D);
+                    tP = (tP + S - tD);
                     lbl_dcto.setText(String.valueOf(tD));
                     lbl_pago.setText(String.valueOf(tP));
                 }
             } else {
-                lbl_dcto.setText(String.valueOf("0"));
-                lbl_pago.setText(String.valueOf(sueldo));
+                lbl_dcto.setText("0");
+                lbl_pago.setText(String.valueOf(S));
             }
         } catch (Exception e) {
             // handle error
